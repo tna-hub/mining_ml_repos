5d7c8deebf29a36655a887bf1b1d726bc3a17134
aqui <aqui@aqui.aqui>
2020-05-29 18:23:03 -0400

    Added requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mining_ml_repos',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Aquilas Tchanjou',
+    author_email='aquilas.tchanjou-njomou@polymtl.ca',
+    description='Code for the paper \'A coin with two sides: How do data and code evolve in Machine Learning projects?\''
+)
